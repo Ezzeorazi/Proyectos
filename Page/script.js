ee07db1876ef3a0debdd832d044e8a49989bd6a3
Ezzeorazi <ezequiel.orazi90@gmail.com>
2023-07-20 17:19:30 -0300

    1739

--- Page/script.js
+++ Page/script.js
@@ -8,3 +8,24 @@ $(window).scroll(function() {
     }
   });
 });
+
+$(document).ready(function() {
+
+  $("#animated-image").hide();
+
+  $(window).scroll(function() {
+    var position = $("#animated-image").offset().top;
+    var scrollPosition = $(window).scrollTop() + $(window).height();
+
+    if (scrollPosition > position) {
+      $("#animated-image").fadeIn(2000);
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var myCarousel = new bootstrap.Carousel(document.getElementById("myCarousel"), {
+    interval: 3000, // Tiempo en milisegundos entre cada cambio de imagen (3 segundos en este caso)
+    wrap: true // Permite que el carrusel continúe reproduciéndose al finalizar la última imagen
+  });
+});
